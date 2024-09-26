0376ffe2910abcfbe6d101b08dd0b1a3e4ce26c3
Mingk42 <xoals123456t@gmail.com>
2024-09-26 11:33:27 +0900

    ml applied

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,9 +1,14 @@
 from typing import Annotated
 from fastapi import FastAPI, UploadFile
+from transformers import pipeline
 
 from hnh.predict import predict
 
+from PIL import Image
+import io
+
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -23,7 +28,18 @@ async def create_upload_file(file: UploadFile):
         print(file.filename)
         pred = predict()
 
+        model = pipeline("image-classification", model="julien-c/hotdog-not-hotdog")
+        #model.save_pretrained("./saved_model")
+
+        img = await file.read()
+        img = Image.open(io.BytesIO(img))
+        p = model(img)
+        print(f"당신이 넣은 이미지는")
+        print(f"{p[0]['score']*100:.2f}%의 확률로 {p[0]['label']}이며")
+        print(f"{p[1]['score']*100:.2f}%의 확률로 {p[1]['label']}입니다.")
+
+
         return {
             "filename": file.filename,
-            "predict": pred
+            "predict": p[0]['label'] if p[0]['score']>p[1]['score'] else p[1]['label']
         }
--- src/hnh/predict.py
+++ src/hnh/predict.py
@@ -6,4 +6,4 @@ def predict():
     rst_list=["핫도그","피자","게살버거","피클","콜라"]
 
 
-    return  rst_list[random.choice(rst_list)]
+    return  random.choice(rst_list)
